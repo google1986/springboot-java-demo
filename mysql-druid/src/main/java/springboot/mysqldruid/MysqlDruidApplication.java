2e8286000435acdd754ced75b742d2c8df7b5bd3
gulianjun <google_zw@163.com>
2020-06-16 14:56:59 +0800

    initial demo

---
+++ mysql-druid/src/main/java/springboot/mysqldruid/MysqlDruidApplication.java
@@ -0,0 +1,13 @@
+package springboot.mysqldruid;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MysqlDruidApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MysqlDruidApplication.class, args);
+    }
+
+}
